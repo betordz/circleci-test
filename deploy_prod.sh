8c48450d430ef2f339145f12294ad4e8af047cff
Jose Castaneda <castaneda@pythian.com>
2017-06-02 12:35:10 -0500

    initial commit

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -4,12 +4,9 @@ ssh -v ec2-user@ec2-54-163-177-62.compute-1.amazonaws.com << EOF
 
 echo '1. Updating sources'
 touch test.txt
-cd /var/www/html/
-git checkout --force master
-git pull
 
 echo "2. Restart apache"
-sudo apache2ctl graceful
+sudo yum install httpd
 
 echo 'Done!'
 
